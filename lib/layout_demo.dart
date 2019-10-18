661625cc86ed4e24f119f00b4cd21c447934d44e
lichenbuliren <617106268@qq.com>
2019-10-18 17:43:13 +0800

    feat(layout_demo): 布局练习

--- lib/layout_demo.dart
+++ lib/layout_demo.dart
@@ -17,45 +17,64 @@ class _LayoutDemoState extends State<LayoutDemo> {
           child: Column(
             children: <Widget>[
               buildTopRow(context),
+              buildTopRow(context),
             ]
           )
         ),
       ),
+      backgroundColor: Colors.white,
     );
   }
 
   Widget buildTopRow(context) {
-    return Row(
-      children: <Widget>[
-        ClipRRect(
-          borderRadius: BorderRadius.circular(8.0),
-          child: Image.asset('assets/icons/googleMaps.png', fit: BoxFit.cover, width: 50, height: 50,),
-        ),
-        Expanded(
-          flex: 1,
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Google Mpas Transition App Google Mpas Transition App Google Mpas Transition App', overflow: TextOverflow.ellipsis,),
-                Text('2019 年 6 月 5 号', style: TextStyle(color: Colors.grey))
-              ]
+    return Padding(
+      padding: EdgeInsets.only(bottom: 10),
+      child: Row(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8.0),
+            child: Image.asset('assets/icons/googleMaps.png', fit: BoxFit.cover, width: 50, height: 50,),
+          ),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Google Mpas Transition App Google Mpas Transition App Google Mpas Transition App', overflow: TextOverflow.ellipsis,),
+                  Text('2019 年 6 月 5 号', style: TextStyle(color: Colors.grey))
+                ]
+              ),
             ),
           ),
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            FlatButton(
-              onPressed: () {},
-              child: Text('OPEN'),
-            )
-          ],
-        )
-      ],
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: Container(
+                  height: 30,
+                  width: 80,
+                  child: FlatButton(
+                    onPressed: () {},
+                    color: Color.fromRGBO(240, 240, 247, 1),
+                    textColor: Colors.blue,
+                    child: Container(
+                      child: Padding(
+                        padding: EdgeInsets.all(4),
+                        child: Text('OPEN', style: TextStyle(fontWeight: FontWeight.bold)),
+                      )
+                    ),
+                  ),
+                )
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 
